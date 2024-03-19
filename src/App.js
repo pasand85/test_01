4bd0571ccba683444372d8d0c97ae20cf97046f6
pasand85 <passos.andreas@gmail.com>
2024-03-19 20:02:42 +0100

    removed empty lines

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Home from "./projects/Blogs1/Home";
 import db2 from "./projects/Blogs1/db2";
 import Home2 from "./projects/Blogs1/Home2";
 
-
 function App() {
 // const title = 'Welcome to the new blog';
 // const likes = 50;
-
-
   const JokesElement = dataJokes.map(joke =>
     {return(
       <Jokes
@@ -18,7 +15,6 @@ function App() {
       />
     )}
     )
-
     // const HomeElement = db2.map((blog) => {
     //   return(
     //     <Home
@@ -27,13 +23,10 @@ function App() {
     //       />
     //   )}
     // )
-
   return (
     <div className="App">
       {/* {JokesElement} */}
-      <Home></Home>
-
-
+      <Home />
 
     </div>
   );
